577e44b7a9447eacc84c1dee683b6d65553fd9a5
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-01-21 02:34:31 -0800

    add some basic draggability of cards

--- media/js/ui.js
+++ media/js/ui.js
@@ -11,3 +11,48 @@ var PlayerView = new Class({
 		$(this.id).getElement('.counter').set('text', this.player.cards.length);
 	}
 });
+
+function make_cards_draggable() {
+	$$('#game .draggable').makeDraggable({
+		droppables: $$('#playing-field'),
+		
+		onEnter: function(draggable, droppable){
+			droppable.setStyle('background', '#E79D35');
+		},
+	
+		onLeave: function(draggable, droppable){
+			droppable.setStyle('background', '#6B7B95');
+		},
+	
+		onDrop: function(draggable, droppable){
+			if (droppable){
+				var offset;
+				switch(draggable.parentNode.get('id')) {
+					case 'player-left':
+						offset = {x: 145, y: 0};
+						break;
+					case 'player-right':
+						offset = {x: -145, y: 0};
+						break;
+					default:
+						console.log(draggable.parentNode.get('id'));
+				}
+				
+				var mover = new Fx.Move(draggable, {
+					relativeTo: draggable.parentNode,
+					position: 'upperLeft',
+					offset: offset
+				});
+				mover.start();
+			} else {
+				// If we don't land on the playing field,
+				// return the card to the deck.
+				var mover = new Fx.Move(draggable, {
+					relativeTo: draggable.parentNode,
+					position: 'upperLeft'
+				});
+				mover.start();
+			}
+		}
+	});
+};
